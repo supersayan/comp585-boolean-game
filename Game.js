4e4cb3778d92e15c0788a969c92d72cff6a39236
Luke Wheeler <deeakronplays@gmail.com>
2021-03-29 19:24:49 -0400

    consolidation of level code

--- Game.js
+++ Game.js
@@ -13,6 +13,12 @@ export default class MainGame extends Phaser.Scene
         }  
         this.fruits;
 
+        //added variables so we don't need to do multiple level .js
+        this.goal11 = 'Fruit';
+        this.goal12 = 'apple';
+        this.goal21 = 'Color';
+        this.goal22 = 'red';
+
         this.circles = new Array(16);
         this.set = []; //just holds the selected fruits
         this.required = []; //holds the required for AND operations
@@ -28,6 +34,18 @@ export default class MainGame extends Phaser.Scene
 
         this.timer; 
         this.timerText;
+    }
+
+    start ()
+    {
+        this.score = 0;
+        this.set = []
+        this.required =[]
+        this.win = false
+
+        //this.timer = this.time.addEvent({ delay: 30000, callback: this.gameOver, callbackScope: this });
+
+        this.sound.play('countdown', { delay: 27 });
     }
 
     create ()
@@ -58,7 +76,7 @@ export default class MainGame extends Phaser.Scene
 
         const fontStyle = {
             fontFamily: 'Arial',
-            fontSize: 48,
+            fontSize: 30,
             color: '#ffffff',
             fontStyle: 'bold',
             padding: 16,
@@ -70,26 +88,85 @@ export default class MainGame extends Phaser.Scene
                 blur: 4
             }
         };
+        
+        const fontStyle2 = {
+            fontFamily: 'Arial',
+            fontSize: 20,
+            color: '#ffffff',
+            fontStyle: 'bold',
+            padding: 8,
+            shadow: {
+                color: '#000000',
+                fill: true,
+                offsetX: 2,
+                offsetY: 2,
+                blur: 4
+            }
+        };
 
-        this.timerText = this.add.text(20, 20, '30:00', fontStyle);
-        this.scoreText = this.add.text(530, 20, 'Hover here to End game', fontStyle);
-        //this.scoreText.setInteractive({ useHandCursor: true});    
-        this.scoreText.on('pointerdown', () => this.gameOver(), this)
+        const fontStyle3 = {
+            fontFamily: 'Impact',
+            fontSize: 25,
+            color: '#ffffff',
+            padding: 6,
+            shadow: {
+                color: '#000000',
+                fill: true,
+                offsetX: 1,
+                offsetY: 1,
+                blur: 1
+            }
+        };
+
+        this.rect3 = this.add.rectangle(0,0, 1600, 250, 0x0000FF, 0.4);
+        //let sprite = this.add.sprite(200,20,"fruits","redapple.png")
+        //sprite.tint = 0x000000;
+        this.fruittext = this.add.text(110, 85, this.goal12, fontStyle3);
+        this.timerText = this.add.text(20, 0, this.goal21 + `   =`, fontStyle2);     
+        this.colorrect = this.add.rectangle(150, 20, 30, 30, 0xFF0000)
+        this.colorrect.setStrokeStyle(2,0x000000);
+        this.timerText2 = this.add.text(70, 45, 'AND', fontStyle3);
+        this.timerText2.setColor('#32CD32');
+        this.timerText = this.add.text(20, 90, this.goal11 + '   =', fontStyle2);
+        this.rect = this.add.rectangle(478, 55, 125, 50,0x55ffff);
+        this.rect.setStrokeStyle(2,0x000000);
+        this.scoreText = this.add.text(410, 20, 'Submit', fontStyle);
+        this.scoreText.setInteractive({ useHandCursor: false});    
+        this.scoreText.once('pointerdown', () => {
+            this.tweens.add({
+                targets: [this.scoreText, this.rect],
+                alpha: {start: 1, to: 0.75},
+                y: '+=5',
+                ease: 'Elastic.out',
+                duration: 100,
+                onComplete: () => {
+                    this.tweens.add({
+                        targets:this.winText,
+                        alpha: {start: 0, to:1}
+                    
+                    })
+                    this.scoreText.disableInteractive();
+                }
+            })
+            this.gameOver()
+        }, this)
+        this.winText = this.add.text(550, 20, 'You Won!', fontStyle);
+
+        this.winText.setAlpha(0);
+        this.loseText = this.add.text(550, 20, 'You Lost...', fontStyle);
+        
+        //this.loseText.setVisible(false);
+        this.loseText.setAlpha(0);
 
         let children = this.fruits.getChildren();
 
         children.forEach((child) => {
 
             child.setInteractive();
-            child.on('gameobjectdown',  () => {
-                this.selectFruit;
-                console.log(pointer);
-                console.log('x');
-            }
-            , this)
+            child.on('gameobjectdown', this.selectFruit, this)
         });
 
-                this.input.on('gameobjectdown', this.selectFruit, this);
+        this.input.on('gameobjectdown', this.selectFruit, this);
         //this.input.once('pointerdown', this.start, this);
 
         this.highscore = this.registry.get('highscore');
@@ -97,16 +174,6 @@ export default class MainGame extends Phaser.Scene
         this.arrangeGrid();
     }
 
-    start ()
-    {
-        this.score = 0;
-        this.set = []
-        this.win = false
-
-        //this.timer = this.time.addEvent({ delay: 30000, callback: this.gameOver, callbackScope: this });
-
-        this.sound.play('countdown', { delay: 27 });
-    }
 
     selectFruit(pointer, fruit)
     {
@@ -160,6 +227,43 @@ export default class MainGame extends Phaser.Scene
         }
     }
 
+    arrangeGrid ()
+    {
+        //  We need to make sure there is only one pair in the grid
+
+        let children = this.fruits.getChildren();
+
+        let a = Math.floor(Math.random()*15)
+        let b = a
+        while (b == a) {
+            b = Math.floor(Math.random()*15)
+        }
+        // boolean expression is pick red and apple
+        // rework this to make it more general
+        children[a].setFrame(this.goal22 + this.goal12 + '.png')
+        children[b].setFrame(this.goal22 + this.goal12 + '.png')
+        this.required.push(a)
+        this.required.push(b)
+
+        for (let i = 0; i < 16; i++)
+        {
+            if (i != a && i != b) {
+                children[i].setFrame(this.game.config.colors[Math.floor((5*Math.random()))] + this.game.config.fruits[Math.floor((4*Math.random()))] +'.png')
+                    if (children[i].frame.customData.color == 'red' && children[i].frame.customData.fruit == 'apple')
+                        this.required.push(i)
+            }
+        }
+
+        //  Stagger tween them all in
+        this.tweens.add({
+            targets: children,
+            scale: { start: 0, from: 0, to: 1 },
+            ease: 'bounce.out',
+            duration: 600,
+            delay: this.tweens.stagger(100, { grid: [ 4, 4 ], from: 'center' })
+        });
+    }
+
     newRound ()
     {
         this.set = []
@@ -178,51 +282,90 @@ export default class MainGame extends Phaser.Scene
         });
     }
 
-    arrangeGrid ()
-    {
+    checkSolution() {
+        let children = this.fruits.getChildren()
+        for (let i = 0; i < this.set.length; i++) {
+            let e = this.set[i]
+            if (!(children[e].frame.customData.color == 'red' && children[e].frame.customData.fruit == 'apple')) {
+                return false
+            }
+        }
+        return true
     }
 
-    checkSolution() {
+    checkSolutionAND() {
+        console.log(this.required)
+        for (let i = 0; i < this.required.length; i++) {
+            let e = this.required[i]
+            if (!(this.set.includes(e))) {
+                console.log(this.set, e)
+                return false
+            }
+        }
         return true
     }
 
     gameOver ()
     {
-        let win = this.checkSolution()
+        let win = (this.checkSolutionAND() && this.checkSolution())
         //  Show them where the match actually was
+        this.input.off('gameobjectdown', this.selectFruit, this);
         if (win) {
             this.win = true;
-            alert('you won')
+            //alert('you won')
+            //this.winText.setVisible(true);
+            this.winText.setColor('#FFD700')
+            let circledance = []
+            for (let i = 0; i < this.set.length; i++){
+                circledance.push(this.circles[this.set[i]])
+            }
+
+            this.score = 0;
+            this.set = []
+            this.required =[]
+            this.win = false
+
+            this.tweens.add({
+                targets: circledance,
+                alpha: 0,
+                yoyo: true,
+                repeat: 2,
+                duration: 250,
+                ease: 'sine.inout',
+                onComplete: () => {
+                    this.input.on('pointerdown', (pointer) => {     
+                        if (pointer.leftButtonDown()) { 
+                            this.input.off('gameobjectdown', this.selectFruit, this);
+                            this.scene.start('MainGame');   
+                        } else if (pointer.rightButtonDown()) {
+                            this.input.once('gameobjectdown', this.selectFruit, this);
+                        }
+                    }, this);
+
+                }
+            });
         }
         else {
-            this.win = false;
-            alert('you lost')
+            this.score = 0;
+            this.set = []
+            this.required =[]
+            this.win = false
+            this.winText = this.loseText;
+            this.winText.setColor('#FF0000')
+            //this.loseText.setVisible(true);
+            //alert('you lost')
+            //Timeout is needed so that the click to submit doesn't count for going to the main menu
+            setTimeout(() => {this.input.on('pointerdown', (pointer) => {
+                if (pointer.leftButtonDown()) { 
+                    this.input.off('gameobjectdown', this.selectFruit, this);
+                    this.scene.start('MainMenu');   
+                } else if (pointer.rightButtonDown()) {
+                    this.input.once('gameobjectdown', this.selectFruit, this);
+                } 
+            }, this)}, 100);
+            
         }
-        
-        this.input.off('gameobjectdown', this.selectFruit, this);
-
-        //console.log(this.score, this.highscore);
-        let circledance = [this.circles[0]]
-        console.log(circledance)
-        for (let i = 0; i < this.set.length; i++){
-            circledance.push(this.circles[this.set[i]])
-        }
-        console.log(circledance)
-        this.tweens.add({
-            targets: circledance,
-            alpha: 0,
-            yoyo: true,
-            repeat: 2,
-            duration: 250,
-            ease: 'sine.inout',
-            onComplete: () => {
-                this.input.once('pointerdown', () => {
-                    this.scene.start('MainMenu');
-                }, this);
-
-            }
-        });
-    }
+   }
 }
 
 function xyConvertToIndex(x,y) {
