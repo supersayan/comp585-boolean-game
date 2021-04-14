5946112b8a6a4d8ae76a1343ceb28c3abbe33933
supersayan <sayandutta135@gmail.com>
2021-04-13 23:31:10 -0400

    level selection

--- main.js
+++ main.js
@@ -4,22 +4,21 @@ import Boot from './Boot.js';
 import Preloader from './Preloader.js';
 import MainMenu from './MainMenu.js';
 import MainGame from './Game.js';
+import LevelSelect from './LevelSelect.js';
 
 const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     backgroundColor: '#008eb0',
-    parent: 'phaser-example',
-    scene: [ Boot, Preloader, MainMenu, MainGame],
-    shapes: ['square', 'triangle', 'circle', 'pentagon', 'trapezoid'],
-    colors: ['red', 'orange', 'green', 'blue', 'purple'],
-    patterns: ['plain', 'striped', 'spots', 'lattice', 'swirl']
+    scene: [ Boot, Preloader, MainMenu, MainGame, LevelSelect],
+    // shapes: ['square', 'triangle', 'circle', 'pentagon', 'trapezoid'],
+    // colors: ['red', 'orange', 'green', 'blue', 'purple'],
+    // patterns: ['plain', 'striped', 'spots', 'lattice', 'swirl']
 };
 
 let game = new Phaser.Game(config);
 //game.config.colors = ['red', 'green', 'blue', 'gray', 'purple']
-//game.config.fruits = ['apple', 'banana', 'orange', 'strawberry']
-game.config.shapes = ['square', 'triangle', 'circle', 'pentagon', 'trapezoid']
-game.config.colors = ['red', 'orange', 'green', 'blue', 'purple']
-game.config.patterns = ['plain', 'striped', 'spotted', 'net', 'spiral']
+// game.config.shapes = ['square', 'triangle', 'circle', 'pentagon', 'trapezoid']
+// game.config.colors = ['red', 'orange', 'green', 'blue', 'purple']
+// game.config.patterns = ['plain', 'striped', 'spotted', 'net', 'spiral']
